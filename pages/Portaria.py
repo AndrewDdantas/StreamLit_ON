9a10a2054c6373477a8785ced5f2fee08c8d3140
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-02-20 12:49:31 +0000

    a

--- pages/Portaria.py
+++ pages/Portaria.py
@@ -115,7 +115,7 @@ merge['PERMANENCIA'] = pd.to_timedelta(merge['PERMANENCIA'], unit='ms')
 merge = merge.fillna('')
 merge['PERMANENCIA'] = merge['PERMANENCIA'].apply(lambda x: '' if x == pd.NaT else x)
 bo = merge
-bo['STT'] = bo.apply(lambda x: 'CRÍTICO' if x['PERMANENCIA'] >= pd.Timedelta(hours=4) else 'ACEITÁVEL')
+bo['STT'] = bo['PERMANENCIA'].apply(lambda x: 'CRÍTICO' if x >= pd.Timedelta(hours=4) else 'ACEITÁVEL')
 bo = bo[bo['STT'] == 'CRÍTICO']
 
 st.dataframe(bo)
