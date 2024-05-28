4a9273641235ada5b624c4660975b2a9ad57ad07
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-05-28 11:34:40 -0300

    Update Fechamento.py

--- pages/Fechamento.py
+++ pages/Fechamento.py
@@ -32,7 +32,7 @@ credentials = ServiceAccountCredentials.from_json_keyfile_dict(
     json, scope)
 client = gs.authorize(credentials)
 
-BASE_STREAMLIT = client.open_by_key('19wq-kacGtgwRS8ZMUpDofA4rpOEMO4r_1SGBtcc7oxM') 
+BASE_STREAMLIT = client.open_by_key(st.secrets['bases']) 
 
 
 base = BASE_STREAMLIT.worksheet('CARTEIRA')
