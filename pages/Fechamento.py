d7ed2131c6e6c851f1fb3ec8e754028d53600bdc
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-08-31 09:45:39 -0300

    Update Fechamento.py

--- pages/Fechamento.py
+++ pages/Fechamento.py
@@ -82,7 +82,7 @@ lot_faturar = carteira_vendas[carteira_vendas['STATUS'] == '6-Conferido Aguardan
 lote_faturar = lot_faturar['NUMLOTE'].nunique()
 
 resultado = carteira_vendas['VALTOTAL'].sum()
-meta = 1945600
+meta = 2122473
 
 
 col1, col2, col3 = st.columns(3)
