103ed807ae033e2bc0384de21b2f0daee584084f
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2025-05-09 10:28:21 -0300

    Deploy v2

---
+++ pages/Preventivo.py
@@ -0,0 +1,207 @@
+import gspread as gs
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import matplotlib.patches as patches
+
+st.set_page_config(
+    page_title="Meu App Streamlit",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",  # Pode ser "wide" ou "centered"
+    initial_sidebar_state="auto",  # Pode ser "auto", "expanded", ou "collapsed"
+)
+
+json = {
+    "type": "service_account",
+    "project_id": st.secrets['project_id'],
+    "private_key_id": st.secrets['KEY'],
+    "private_key": st.secrets['private_key'],
+    "client_email": st.secrets['client_email'],
+    "client_id": st.secrets['client_id'],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/case-693%40digital-layout-402513.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+    }
+
+scope = ['https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive'] 
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+    json, scope)
+client = gs.authorize(credentials)
+
+sheet = client.open_by_key(st.secrets['preventivo'])
+worksheet = sheet.worksheet('Base SQL')
+
+
+data = worksheet.get_values('a:p')
+
+df = pd.DataFrame(data)
+
+calculadora = sheet.worksheet('Cal NS')
+df2 = calculadora.get_values('c3:C7')
+
+columns = df.iloc[:1].values.tolist()[0]
+df.columns = columns
+df = df.drop(index=0)
+df['A Vencer'] = df.apply(lambda x: 1 if (x['FINALIZADOR'] == 'PENDENTE') & (x['STATUS PRAZO'] == 'À VENCER') else 0, axis=1)
+df['Vencido'] = df.apply(lambda x: 1 if (x['FINALIZADOR'] == 'PENDENTE') & (x['STATUS PRAZO'] == 'VENCIDO') else 0, axis=1)
+df['Vence Hoje'] = df.apply(lambda x: 1 if (x['FINALIZADOR'] == 'PENDENTE') & (x['STATUS PRAZO'] == 'VENCE HOJE') else 0, axis=1)
+df['Total'] = df.apply(lambda x: 1 if (x['FINALIZADOR'] == 'PENDENTE') else 0, axis=1)
+d2 = df.loc[(df['REGIONAL']!='CORREIOS') & (df['REGIONAL']!='FRACIONADO')] 
+df = df.loc[(df['REGIONAL']!='CORREIOS') & (df['REGIONAL']!='FRACIONADO') & (df['FINALIZADOR'] == 'PENDENTE')]
+def var(x):
+    if x == 'Endere o do Cliente Destino n o Localizado': 
+        return 'Endereço Não_Localizado'
+    elif x == 'Em rota de entrega':
+        return 'Em Entrega'
+    elif x == '1a Tentativa Cliente Ausente':
+        return '1a_Tentativa Cliente_Ausente'
+    elif x == 'Saiu para Entrega':
+        return 'Saiu_para Entrega'
+    elif x == 'Troca de transportadora':
+        return 'Troca transportadora'
+
+df['STATUS FINAL'] = df['STATUS FINAL'].apply(var)
+df.sort_values('STATUS FINAL', ascending=False)
+
+dina = df.loc[(df['STATUS FINAL'] != '') & (df['STATUS PRAZO'] == 'VENCE HOJE')]
+dina = dina.groupby(['TRANSP CORRIGIDA', 'STATUS FINAL']).agg({'Total': 'sum'}).fillna(0).reset_index()
+din = pd.pivot_table(dina, values='Total', index='TRANSP CORRIGIDA', columns='STATUS FINAL', aggfunc='sum', fill_value=0)
+din = din[din.sum().sort_values(ascending=False).index]
+din = din.iloc[:, :5]
+data = pd.DataFrame(din)
+data = data.head(16)
+try:
+    data = data.reset_index().sort_values('Em Entrega', ascending=False)
+except:
+    data = data.reset_index()
+trans = df.groupby('TRANSP CORRIGIDA').agg({'A Vencer': 'sum', 'Vence Hoje': 'sum', 'Vencido': 'sum', 'Total': 'sum'}).reset_index()
+total = trans['Total'].sum()
+a_vence = trans['A Vencer'].sum()
+vence = trans['Vence Hoje'].sum()
+venci = trans['Vencido'].sum()
+trans = trans.sort_values(['Vence Hoje', 'Vencido'], ascending=False).head(26) # Grid para encaixe de Dashboard.
+
+fig = plt.figure(layout="constrained", figsize=(30, 12))
+
+gs = GridSpec(3, 6, figure=fig)
+ax1 = fig.add_subplot(gs[:1, :3])
+ax2 = fig.add_subplot(gs[0:3, 3:-1])
+ax3 = fig.add_subplot(gs[1:3, :-3])
+ax4 = fig.add_subplot(gs[0:3, 5:])
+
+rectangle1 = patches.Rectangle((0.15, 0.2), 0.3, 0.3, linewidth=1, edgecolor='none', facecolor='#ED7D31')
+rectangle2 = patches.Rectangle((0.15, 0.6), 0.3, 0.3, linewidth=1, edgecolor='none', facecolor='#00B0F0')
+rectangle3 = patches.Rectangle((0.55, 0.2), 0.3, 0.3, linewidth=1, edgecolor='none', facecolor='red')
+rectangle4 = patches.Rectangle((0.55, 0.6), 0.3, 0.3, linewidth=1, edgecolor='none', facecolor='green')
+
+ax1.add_patch(rectangle1)
+ax1.add_patch(rectangle2)
+ax1.add_patch(rectangle3)
+ax1.add_patch(rectangle4)
+
+rectangle1 = patches.Rectangle((0, 0.81), 1, 0.17, linewidth=1, edgecolor='none', facecolor='#00B0F0')
+rectangle2 = patches.Rectangle((0, 0.61), 1, 0.17, linewidth=1, edgecolor='none', facecolor='#00B0F0')
+rectangle3 = patches.Rectangle((0, 0.41), 1, 0.17, linewidth=1, edgecolor='none', facecolor='#00B0F0')
+rectangle4 = patches.Rectangle((0, 0.21), 1, 0.17, linewidth=1, edgecolor='none', facecolor='#00B0F0')
+rectangle5 = patches.Rectangle((0, 0.01), 1, 0.17, linewidth=1, edgecolor='none', facecolor='#00B0F0')
+
+ax4.add_patch(rectangle1)
+ax4.add_patch(rectangle2)
+ax4.add_patch(rectangle3)
+ax4.add_patch(rectangle4)
+ax4.add_patch(rectangle5)
+
+ax1.text(0.3, 0.75, f'Total Pedidos\n{total}', ha='center', va='center', fontsize=25, color='white', weight='bold')
+ax1.text(0.3, 0.35, f'Vence Hoje\n{vence}', ha='center', va='center', fontsize=25, color='white', weight='bold')
+ax1.text(0.7, 0.75, f'A Vencer\n{a_vence}', ha='center', va='center', fontsize=25, color='white', weight='bold')
+ax1.text(0.7, 0.35, f'Vencidos\n{venci}', ha='center', va='center', fontsize=25, color='white', weight='bold')
+
+ax4.text(0.5, 0.89, f'Total Pedidos\n{df2[0][0]}', ha='center', va='center', fontsize=40, color='white', weight='bold')
+ax4.text(0.5, 0.69, f'Pendentes\n{df2[1][0]}', ha='center', va='center', fontsize=40, color='white', weight='bold')
+ax4.text(0.5, 0.49, f'%NS\n{df2[2][0]}', ha='center', va='center', fontsize=40, color='white', weight='bold')
+ax4.text(0.5, 0.29, f'Saldo Meta (%)\n{df2[3][0]}', ha='center', va='center', fontsize=30, color='white', weight='bold')
+ax4.text(0.5, 0.09, f'Saldo de Pendentes\npara meta(Pedidos)\n{df2[4][0]}', ha='center', va='center', fontsize=30, color='white', weight='bold')
+
+ax1.axis('off')
+ax2.axis('off')
+ax3.axis('off')
+ax4.axis('off')
+
+tabela1 = ax2.table(cellText=trans.values, colLabels=trans.columns, loc='upper left')
+tabela1.auto_set_font_size(False)
+tabela1.set_fontsize(15)
+tabela1.scale(1, 2)
+
+for (i,j), cell in tabela1._cells.items():
+    cell.get_text().set_ha('center')
+    if i == 0:
+        if j == 1:
+            cell.set_fontsize(17)
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor('green')
+            cell.get_text().set_ha('center')
+        elif j == 2:
+            cell.set_fontsize(17)
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor('#ED7D31')
+            cell.get_text().set_ha('center')
+        elif j == 3:
+            cell.set_fontsize(17)
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor('red')
+            cell.get_text().set_ha('center')
+        else:
+            cell.set_fontsize(17)
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor('#00B0F0')
+            cell.get_text().set_ha('center')
+
+    else:
+        cell.set_text_props(weight='bold', color='black')
+
+
+for i in range(len(trans)+1):
+    for o in range(len(trans.columns)):
+        if o > 0:
+            cell = tabela1[(i, o)]
+            cell.set_width(0.17)
+        else:
+            cell = tabela1[(i, 0)]
+            cell.set_width(0.3)
+
+
+
+tabela2 = ax3.table(cellText=data.values, colLabels=data.columns, loc='upper left')
+tabela2.auto_set_font_size(False)
+tabela2.set_fontsize(15)
+tabela2.scale(1, 2)
+
+
+for key, cell in tabela2.get_celld().items():
+    if key[0] == 0:
+        cell.get_text().set_text('\n'.join(cell.get_text().get_text().split()))
+
+for (i,j), cell in tabela2._cells.items():
+    cell.get_text().set_ha('center')
+    if i == 0:
+        cell.set_fontsize(17)
+        cell.set_text_props(weight='bold', color='w')
+        cell.set_facecolor('#00B0F0')
+        cell.get_text().set_ha('center')
+    else:
+        cell.set_text_props(weight='bold', color='black')
+
+for o in range(min(len(data), len(data.columns))):
+    cell = tabela2[(0, o)]
+    cell.set_height(0.1)
+
+
+fig.suptitle("Preventivo Entregas", fontsize=40)
+
+
+st.pyplot(fig)
---
+++ pages/Produção.py
@@ -0,0 +1,347 @@
+import locale as lo
+import gspread as gs
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta 
+import streamlit as st
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+def term(ax, atual, alvo, data):
+
+    if atual / alvo <= 0.2:
+        cor_termometro_cheio = 'red'
+    elif atual / alvo <= 0.7:
+        cor_termometro_cheio = 'yellow'
+    else:
+        cor_termometro_cheio = 'green'
+
+    # Adicionando barras do termômetro
+    ax.barh(0.5, (atual / alvo) * 100, 1, color=cor_termometro_cheio, edgecolor='black')
+    ax.barh(0.5, (100 - (atual / alvo) * 100), 1, left=(atual / alvo) * 100, color='lightgray', edgecolor='black')
+
+    # Ajustando os limites dos eixos
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    faltando = alvo - atual
+    faltando = fmt_num(faltando, 'NORMAL', 1)
+    alvo = fmt_num(alvo, 'NORMAL', 1)
+    atual = fmt_num(atual, 'NORMAL', 1)
+    # Adicionando texto
+    ax.text(50, 1.2, f'Programado: {alvo}      Separado: {atual}       Faltando: {faltando}',
+            ha='center', va='center', fontsize=20)
+    ax.text(50, 1.2+0.25, f'{data}',
+            ha='center', va='center', fontsize=30)
+
+def table(ax, dados):
+    ax.axis('off')
+    tabela = ax.table(cellText=dados.values, colLabels=dados.columns, loc='upper left')
+    tabela.auto_set_font_size(False)
+    tabela.auto_set_column_width(col=list(range(len(dados.columns))))
+    tabela.set_fontsize(14)
+    tabela.scale(1,3)
+    ax.set_ylim(-10000, 100000)
+    for (i,j), cell in tabela._cells.items():
+        cell.get_text().set_ha('center')
+        if i == 0 or i ==  max(key[0] for key in tabela._cells.keys()):
+            cell.set_fontsize(14)
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor('#00B0F0')
+            cell.get_text().set_ha('center')
+        else:
+            cell.set_text_props(weight='bold', color='black')
+
+st.set_page_config(
+    page_title="Produção",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",  # Pode ser "wide" ou "centered"
+    initial_sidebar_state="auto",  # Pode ser "auto", "expanded", ou "collapsed"
+)
+
+with open('style.css', 'r') as f: #abre o css
+    css_string = f.read()
+
+
+# Injetar o CSS no Streamlit
+st.markdown(f'<style>{css_string}</style>', unsafe_allow_html=True)
+# funções 
+
+def fmt_num(valor, tipo, casas=0): # Função para formatar números.
+    if tipo == 'REAL':
+        return lo.format_string(f"R$ %0.{casas}f",valor,grouping=True)
+    if tipo == 'CUBAGEM':
+        return "{:,.1f}".format(valor).replace(',', 'X').replace('.', ',').replace('X', '.')
+    if tipo == 'NORMAL':
+        return "{:,.0f}".format(valor).replace(',', 'X').replace('.', ',').replace('X', '.')
+    if tipo == "PORCENTAGEM":
+        return f"{{:.{casas}%}}".format(valor).replace('.',',')
+
+json = {
+    "type": "service_account",
+    "project_id": st.secrets['project_id'],
+    "private_key_id": st.secrets['KEY'],
+    "private_key": st.secrets['private_key'].replace("\\n", "\n"),
+    "client_email": st.secrets['client_email'],
+    "client_id": st.secrets['client_id'],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/case-693%40digital-layout-402513.iam.gserviceaccount.com",
+}
+scope = ['https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive'] 
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+    json, scope)
+client = gs.authorize(credentials)
+
+# Baixando informações da grade
+planilha = client.open_by_key(st.secrets['grade']) 
+grade = planilha.worksheet('Dados_consolidado')
+dados_grade = grade.get_values('b3:l')
+dados_grade = pd.DataFrame(dados_grade)
+dados_grade.columns = ['DTPROGRAMACAO', 'STATUS', 'OPERACAO', 'ID_CARGA', 'DTOFERECIMENTO', 'HROFERECIMENTO', 'Carregamento', 'FILIAL',
+                                    'ROTA', 'LOTE', 'TURNO']
+dados_grade = dados_grade.loc[dados_grade['DTPROGRAMACAO'] != '']
+dados_grade['LOTE'] = dados_grade['LOTE'].astype(str)
+dados_grade['DTPROGRAMACAO'] = dados_grade['DTPROGRAMACAO'] + '/2024' 
+dados_grade['DTPROGRAMACAO'] = pd.to_datetime(dados_grade['DTPROGRAMACAO'], format='%d/%m/%Y')
+dados_grade = dados_grade.sort_values('DTPROGRAMACAO')
+
+BASE_STREAMLIT = client.open_by_key(st.secrets['bases']) 
+base = BASE_STREAMLIT.worksheet('STATUS_OPERAÇÃO')
+base = base.get_values('A2:N')
+wis = pd.DataFrame(base)
+wis.columns = ['DATA_CORTE','LOTE','PROGRAMACAO','CUB_PROGRAMADA','PEDIDOS','CD_SITUACAO','SEPARADO','CUB_SEPARADA','CONFERIDO','CUB_CONFERIDA','PENDENTE_SEP','CUB_PENDENTE_SEP','PENDENTE_CONF','CUB_PENDENTE_CONF']
+wis['LOTE'] = wis['LOTE'].astype(str)
+wis[['PROGRAMACAO','CUB_PROGRAMADA','PEDIDOS','SEPARADO','CUB_SEPARADA','CONFERIDO','CUB_CONFERIDA','PENDENTE_SEP','CUB_PENDENTE_SEP','PENDENTE_CONF','CUB_PENDENTE_CONF']] = wis[['PROGRAMACAO','CUB_PROGRAMADA','PEDIDOS','SEPARADO','CUB_SEPARADA','CONFERIDO','CUB_CONFERIDA','PENDENTE_SEP','CUB_PENDENTE_SEP','PENDENTE_CONF','CUB_PENDENTE_CONF']].apply(lambda x: x.str.replace(',','.')).astype(float)
+
+join_dados = pd.merge(wis,dados_grade,how="inner", on="LOTE")
+  
+def definir_status(row):
+    if (row['PROGRAMACAO'] - row['CONFERIDO'] <= 0) or (row['CD_SITUACAO'] in ['60.0', '68.0']):
+        return 'SEPARADO'
+    else:
+        return 'PENDENTE'
+
+join_dados['STATUS_AJUSTADO'] = join_dados.apply(definir_status, axis=1)
+
+pendente = join_dados[join_dados['STATUS_AJUSTADO'] == 'PENDENTE']
+
+pendente = pendente['DTPROGRAMACAO'].sort_values().unique()
+
+hr = join_dados.groupby(['DTPROGRAMACAO','HROFERECIMENTO','DTOFERECIMENTO']).agg({'ID_CARGA':'nunique', 'PROGRAMACAO':'sum', 'CUB_PROGRAMADA': 'sum', 'SEPARADO':'sum','CONFERIDO':'sum', 'CUB_SEPARADA':'sum','CUB_CONFERIDA':'sum'}).reset_index()
+hr = hr.loc[hr['DTPROGRAMACAO'].isin(pendente)]
+hr['OFERECIMENTO'] = hr['DTOFERECIMENTO'] +' '+hr['HROFERECIMENTO']
+hr = hr[['DTPROGRAMACAO','ID_CARGA','OFERECIMENTO', 'PROGRAMACAO', 'CUB_PROGRAMADA', 'SEPARADO', 'CUB_SEPARADA','CONFERIDO','CUB_CONFERIDA']]
+hr['OFERECIMENTO'] = pd.to_datetime(hr['OFERECIMENTO'], format='%d/%m/%Y %H:%M')
+hr = hr.sort_values('OFERECIMENTO')
+hr['OFERECIMENTO'] = hr['OFERECIMENTO'].dt.time
+hr['DTPROGRAMACAO'] = hr['DTPROGRAMACAO'].dt.strftime('%d/%m/%Y')
+data_dist = pendente.strftime('%d/%m/%Y')
+
+
+v = 5*len(data_dist)
+if len(data_dist) > 3: 
+    tea = 25+v
+else:
+    tea = 20+v
+fig = plt.figure(figsize=(35, tea))
+
+
+i = 1
+c = 0
+r = 1
+t = 3
+while i <= len(data_dist):
+    
+    gs1 = GridSpec(3*len(data_dist), 4, wspace=1, hspace=1)
+    ax1 = fig.add_subplot(gs1[c:r, :2])
+    ax2 = fig.add_subplot(gs1[r:t, :2])
+    ax3 = fig.add_subplot(gs1[c:r, 2:4])
+    ax4 = fig.add_subplot(gs1[r:t, 2:4])
+    te = hr.loc[hr['DTPROGRAMACAO'] == data_dist[i-1]]
+    term(ax1, te['SEPARADO'].sum(), te['PROGRAMACAO'].sum(), data_dist[i-1])
+    term(ax3, te['CUB_SEPARADA'].sum(), te['CUB_PROGRAMADA'].sum(), data_dist[i-1])
+
+    
+    pecas = te[['DTPROGRAMACAO','OFERECIMENTO','ID_CARGA', 'PROGRAMACAO', 'SEPARADO','CONFERIDO']]
+    pecas["% SEP"] = pecas['SEPARADO'] / pecas['PROGRAMACAO']
+    pecas["% CONF"] = pecas['CONFERIDO'] / pecas['PROGRAMACAO']
+    pecas['PEND SEP'] = pecas['PROGRAMACAO'] - pecas['SEPARADO'] 
+    pecas['PEND CONF'] = pecas['PROGRAMACAO'] - pecas['CONFERIDO']
+    total_pecas = pecas[['ID_CARGA','PROGRAMACAO', 'SEPARADO','CONFERIDO','PEND SEP','PEND CONF']].sum()
+    media_pecas = pecas[["% SEP","% CONF"]].mean()
+
+    total_row = pd.DataFrame([['Total']+['-'] + total_pecas.tolist()+media_pecas.tolist()])
+    total_row = total_row[[0,1,2,3,4,6,8,5,7,9]]
+    total_row.columns = ['DTPROGRAMACAO','OFERECIMENTO', 'ID_CARGA', 'PROGRAMACAO', 'SEPARADO','PEND SEP','% SEP','CONFERIDO','PEND CONF','% CONF']
+
+    pecas = pecas[['DTPROGRAMACAO', 'OFERECIMENTO', 'ID_CARGA', 'PROGRAMACAO', 'SEPARADO','PEND SEP','% SEP','CONFERIDO','PEND CONF','% CONF']]
+    pecas = pd.concat([pecas,total_row], ignore_index=True)
+    
+    pecas.loc[:, '% SEP'] = pecas['% SEP'].apply(fmt_num, tipo='PORCENTAGEM', casas=1)
+    pecas.loc[:, '% CONF'] = pecas['% CONF'].apply(fmt_num, tipo='PORCENTAGEM', casas=1)
+    pecas.loc[:, 'PROGRAMACAO'] = pecas['PROGRAMACAO'].apply(fmt_num, tipo='NORMAL')
+    pecas.loc[:, 'SEPARADO'] = pecas['SEPARADO'].apply(fmt_num, tipo='NORMAL')
+    
+    pecas.columns = ['DT_PROG', 'OFEREC',  'CARGAS','PEÇAS_PROG', 'SEPARADO', 'PEND SEP','% SEP', 'CONFERIDO','PEND CONF','% CONF']
+
+    cubagem = te[['CUB_PROGRAMADA', 'CUB_SEPARADA','CUB_CONFERIDA']]
+    cubagem['% SEP'] = cubagem['CUB_SEPARADA'] / cubagem['CUB_PROGRAMADA']
+    cubagem['% CONF'] = cubagem['CUB_CONFERIDA'] / cubagem['CUB_PROGRAMADA']
+    cubagem['PEND SEP'] = cubagem['CUB_PROGRAMADA'] - cubagem['CUB_SEPARADA']
+    cubagem['PEND CONF'] = cubagem['CUB_PROGRAMADA'] - cubagem['CUB_CONFERIDA'] 
+    cubagem_total = cubagem[['CUB_PROGRAMADA', 'CUB_SEPARADA','PEND SEP','CUB_CONFERIDA','PEND CONF']].sum()
+    cubagem_media = cubagem[['% SEP','% CONF']].mean()
+    cubagem_row = pd.DataFrame([cubagem_total.tolist()+cubagem_media.tolist()])
+    cubagem_row = cubagem_row[[0,1,2,5,3,4,6]]
+    cubagem_row.columns = ['CUB_PROGRAMADA', 'CUB_SEPARADA','PEND SEP','% SEP','CUB_CONFERIDA','PEND CONF','% CONF']
+    cubagem = pd.concat([cubagem,cubagem_row], ignore_index=True)
+    
+    cubagem.loc[:, 'CUB_CONFERIDA'] = cubagem['CUB_CONFERIDA'].apply(fmt_num, tipo='CUBAGEM', casas=2)
+    cubagem.loc[:, '% SEP'] = cubagem['% SEP'].apply(fmt_num, tipo='PORCENTAGEM', casas=1)
+    cubagem.loc[:, '% CONF'] = cubagem['% CONF'].apply(fmt_num, tipo='PORCENTAGEM', casas=1)
+    cubagem.loc[:, 'CUB_PROGRAMADA'] = cubagem['CUB_PROGRAMADA'].apply(fmt_num, tipo='CUBAGEM', casas=2)
+    cubagem.loc[:, 'CUB_SEPARADA'] = cubagem['CUB_SEPARADA'].apply(fmt_num, tipo='CUBAGEM', casas=2)
+    cubagem.loc[:, 'PEND SEP'] = cubagem['PEND SEP'].apply(fmt_num, tipo='CUBAGEM', casas=2)
+    cubagem.loc[:, 'PEND CONF'] = cubagem['PEND CONF'].apply(fmt_num, tipo='CUBAGEM', casas=2)
+    cubagem = cubagem[['CUB_PROGRAMADA', 'CUB_SEPARADA','PEND SEP','% SEP','CUB_CONFERIDA','PEND CONF','% CONF']]
+
+    table(ax2, pecas)
+    table(ax4, cubagem)
+    
+    i += 1
+    c = c+3
+    r = r+3
+    t = t+3
+st.text('Status Produção CD 2650')
+st.pyplot(fig)
+
+# Rort produção por turno.
+base = BASE_STREAMLIT.worksheet('PRODUÇÃO_TURNO')
+base = base.get_values('A2:F')
+wis_turno = pd.DataFrame(base)
+wis_turno.columns = ['DT_COMPETENCIA','LOTE','TURNO','QT_SEPARADO','CUBAGEM','HORA']
+wis_turno['LOTE'] = wis_turno['LOTE'].astype(str)
+wis_turno[['QT_SEPARADO','CUBAGEM']] = wis_turno[['QT_SEPARADO','CUBAGEM']].apply(lambda x: x.str.replace(',','.')).astype(float)
+join_turno = pd.merge(wis_turno,dados_grade,how='inner', on='LOTE')
+
+join_turno['DTPROGRAMACAO'] = pd.to_datetime(join_turno['DTPROGRAMACAO'], format='%d/%m/%Y')
+condicao = [(join_turno['DTPROGRAMACAO'] == join_turno['DT_COMPETENCIA']),
+            (join_turno['DTPROGRAMACAO'] > join_turno['DT_COMPETENCIA']),
+            (join_turno['DTPROGRAMACAO'] < join_turno['DT_COMPETENCIA'])]
+
+opcap = ['PRODUÇÃO\nDIA','ADIANTADO','ATRASADO']
+opcap2 = ['b','c','a']
+join_turno['STATUS'] = np.select(condicao,opcap)
+join_turno['sort'] = np.select(condicao,opcap2)
+
+hoje = datetime.now() - timedelta(hours=3)
+h = int(hoje.strftime('%H'))
+if h <= 6:
+    hoje = datetime.now() - timedelta(1, hours=3)
+else:
+    hoje = datetime.now() - timedelta(hours=3)
+data_hoje = hoje.strftime('%Y-%m-%d')
+join_turno = join_turno.loc[join_turno['DT_COMPETENCIA'] == data_hoje]
+
+som = join_turno.groupby(['HORA']).agg({'QT_SEPARADO': 'sum', 'CUBAGEM': 'sum'}).reset_index()
+som['HORA'] = som['HORA'].astype(int)
+som = som.sort_values('HORA')
+soma_m = join_turno.groupby(['TURNO_x','DT_COMPETENCIA','STATUS','sort']).agg({'QT_SEPARADO': 'sum', 'CUBAGEM': 'sum'}).reset_index().sort_values('sort')
+soma_m = soma_m[['TURNO_x','DT_COMPETENCIA','STATUS','QT_SEPARADO','CUBAGEM']]
+
+unique_data = soma_m['DT_COMPETENCIA'].unique()
+o = 0
+
+
+for i in unique_data:
+    o += 1
+    dt = i
+    soma_l = soma_m[soma_m['DT_COMPETENCIA'] == dt]
+    soma_f = soma_m.groupby(['TURNO_x']).agg({'QT_SEPARADO': 'sum', 'CUBAGEM': 'sum'}).reset_index()
+    
+    fig, axs = plt.subplots(2, len(soma_f), figsize=(len(soma_f)*5, 10))
+    
+    for i, (_, row) in enumerate(soma_f.iterrows()):
+        turno = row['TURNO_x']
+        data = pd.to_datetime(soma_l['DT_COMPETENCIA'].iloc[0], format='%Y-%m-%d').strftime('%d/%m/%Y')
+        turno_df = soma_l[soma_l['TURNO_x'] == turno]
+
+        # Plot QT_SEPARADO
+        if len(soma_f) == 1:
+            ax1 = axs[0]
+        else:
+            ax1 = axs[0, i]
+        ax1.bar(turno_df['STATUS'], turno_df['QT_SEPARADO'])
+        ax1.grid(False)
+        for index, value in enumerate(turno_df['QT_SEPARADO']):
+            valu = fmt_num(value,'NORMAL')
+            ax1.text(index, value, f'{valu}', ha='center', va='bottom')
+        ax1.set_title(f'{turno} do dia {data}')
+        y = max(turno_df['QT_SEPARADO'])*1.2
+        ax1.set_ylim(0, y)
+        smm = turno_df['QT_SEPARADO'].sum()
+        smm = fmt_num(smm, 'NORMAL')
+        ax1.legend([smm])
+
+        # Plot CUBAGEM
+        if len(soma_f) == 1:
+            ax2 = axs[1]
+        else:
+            ax2 = axs[1,i]
+        
+        ax2.bar(turno_df['STATUS'], turno_df['CUBAGEM'])
+        ax2.grid(False)
+        for index, value in enumerate(turno_df['CUBAGEM']):
+            valu = fmt_num(value,'CUBAGEM',1)
+            ax2.text(index, value, f'{valu}', ha='center', va='bottom')
+        ax2.set_title(f'{turno} do dia {data}')
+        y = max(turno_df['CUBAGEM'])*1.2
+        ax2.set_ylim(0, y)
+        smm = turno_df['CUBAGEM'].sum()
+        smm = fmt_num(smm, 'CUBAGEM', 1)
+        ax2.legend([smm])
+
+
+    plt.tight_layout()
+col1, col2 = st.columns(2)
+col1.pyplot(fig)
+
+fig, ax = plt.subplots(2, 1, figsize=(2+len(som['QT_SEPARADO'])*0.9, 10))
+ax[0].bar(som['HORA'].astype(str),som['QT_SEPARADO'])
+ax[0].grid(False)
+y = max(som['QT_SEPARADO'])*1.2
+ax[0].set_ylim(0,y)
+for index, val in enumerate(som['QT_SEPARADO']):
+    valu = fmt_num(val, 'NORMAL')
+    ax[0].text(index, val, f'{valu}', ha='center', va='bottom')
+avg = fmt_num(som['QT_SEPARADO'].mean(), 'NORMAL',1)
+avc = som['QT_SEPARADO'].mean()
+smm = som['QT_SEPARADO'].sum()
+ax[0].axhline(y=avc, color='r', linestyle='--', label=f'Média: {avg}')
+ax[0].legend([f'Média {avg}',f'Total {smm}'],loc ="upper left")
+ax[0].set_title(f'Peças')
+
+y = max(som['CUBAGEM'])*1.2
+ax[1].bar(som['HORA'].astype(str),som['CUBAGEM'])
+ax[1].set_ylim(0,y)
+ax[1].grid(False)
+avg = fmt_num(som['CUBAGEM'].mean(), 'NORMAL',1)
+avc = som['CUBAGEM'].mean()
+smm = fmt_num(som['CUBAGEM'].sum(), 'NORMAL',1)
+
+for index, val in enumerate(som['CUBAGEM']):
+    valu = fmt_num(val, 'CUBAGEM', 1)
+    ax[1].text(index, val, f'{valu}', ha='center', va='bottom')
+ax[1].axhline(y=avc, color='r', linestyle='--', label=f'Média: {avg}')
+ax[1].legend([avg,smm],loc ="upper left")
+ax[1].set_title(f'Cubagem')
+fig.suptitle(f'Acompanhamento hora a hora {data}')
+
+col2.pyplot(fig)
+
+
